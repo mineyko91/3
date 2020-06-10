d857f6785490f9149257fc9b6bd91a3b30cc82fd
Artsiom Mineika <mineyko91@gmail.com>
2020-06-10 22:41:00 +0300

    initial commit

---
+++ MoveResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _3
+{
+    internal class MoveResult
+    {
+        public string Name { get; set; }
+
+        public uint MoveIndex { get; }
+
+        public string Move { get; }
+
+        public byte[] HmacKey { get; }
+
+        public HMAC Hmac { get; }
+
+        public MoveResult(string name, string[] move, uint choice)
+        {
+            Name = name;
+            MoveIndex = choice;
+            Move = move[MoveIndex];
+            HmacKey = GetRandomKey();
+            Hmac = HMAC.Create("HMACSHA256");
+            Hmac.Key = HmacKey;
+        }
+
+        public string GetHMAC()
+            => BitConverter.ToString(Hmac.ComputeHash(Encoding.UTF8.GetBytes(Move)))
+            .Replace("-", "").ToLower();
+
+        public string GetHMACKey()
+            => BitConverter.ToString(HmacKey)
+            .Replace("-", "");
+
+        private static byte[] GetRandomKey()
+        {
+            var bytes = new byte[64];
+            RandomNumberGenerator.Create()
+                .GetBytes(bytes);
+
+            return bytes;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] moves = GetInput(args);
+
+            MoveResult aiMove = AIMove("Script", moves);
+
+            Console.WriteLine($"HMAC: {aiMove.GetHMAC()}");
+            ShowMenu(moves);
+
+            MoveResult myMove = PlayerMove("Player", moves);
+
+            Console.WriteLine($"Player move: {myMove.Move}  \n" +
+                              $"Computer move: {aiMove.Move} \n" +
+                              $"Result: {GetResult(myMove, aiMove, moves)} \n" +
+                              $"HMAC key: {aiMove.GetHMACKey()}");
+
+            GetResult(myMove, aiMove, moves);
+
+            Console.ReadKey();
+        }
+
+        private static string GetResult(MoveResult firstMove, MoveResult secondMove, string[] moves)
+        {
+            int winingCount = (moves.Length - 1) / 2;
+
+            if (firstMove.MoveIndex == secondMove.MoveIndex)
+            {
+                return "draw";
+            }
+
+            for (uint i = firstMove.MoveIndex + 1, j = 0; j < winingCount; i++, j++)
+            {
+                if (i > moves.Length - 1)
+                {
+                    i = 0;
+                }
+
+                if (i == secondMove.MoveIndex)
+                {
+                    return secondMove.Name + " win!";
+                }
+            }
+
+            return firstMove.Name + " win!";
+        }
+
+        private static void ShowMenu(string[] moves)
+        {
+            Console.WriteLine("Available moves:");
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} - {moves[i]}");
+            }
+
+            Console.WriteLine("0 - exit \n" +
+                "Enter your move: ");
+        }
+
+        private static MoveResult PlayerMove(string name, string[] moves)
+        {
+            uint choice;
+            while (!uint.TryParse(Console.ReadLine(), out choice) || choice > moves.Length)
+            {
+                ShowMenu(moves);
+            }
+
+            if (choice == 0)
+            {
+                Environment.Exit(0);
+            }
+
+            return new MoveResult(name, moves, choice - 1);
+        }
+
+        private static MoveResult AIMove(string name, string[] moves)
+            => new MoveResult(name, moves, (uint)new Random().Next(0, moves.Length));
+
+        private static string[] GetInput(string[] args)
+        {
+            if (args.Length < 3 || args.Length % 2 == 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return args;
+            }
+        }
+    }
+}
